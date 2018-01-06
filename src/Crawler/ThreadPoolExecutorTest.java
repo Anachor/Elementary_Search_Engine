d9032b121bb262c1f6cdef78c0d6b40e6f605bcb
Anachor <anachored@gmail.com>
2018-01-07 01:36:34 +0600

    Testing

--- src/Crawler/ThreadPoolExecutorTest.java
+++ src/Crawler/ThreadPoolExecutorTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 public class ThreadPoolExecutorTest {
     public static void main(String[] args) {
-        ThreadPoolExecutor threadPoolExecutor = Executors.newFixedThreadPool(10);
+        //ThreadPoolExecutor threadPoolExecutor = Executors.newFixedThreadPool(10);
+        System.out.println("Test");
     }
 }
