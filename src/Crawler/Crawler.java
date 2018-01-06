14d6fbbc1d876cbf47a58f95a85ea7b5326f066c
Anachor <anachored@gmail.com>
2018-01-07 02:26:21 +0600

    Testing

--- src/Crawler/Crawler.java
+++ src/Crawler/Crawler.java
@@ -3,6 +3,7 @@ package Crawler;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Crawler {
     HashMap<String, String> urlCacheHash;
@@ -11,8 +12,8 @@ public class Crawler {
     String[] rootURLs;
 
     public Crawler(String[] rootURLs) {
-        urlCacheHash = new HashMap<String, String>();
-        urlCount = new HashMap<String, Integer>();
+        urlCacheHash = new HashMap<>();
+        urlCount = new HashMap<>();
         urlQueue = new LinkedList<URLTrial>();
         this.rootURLs = rootURLs;
     }
@@ -27,21 +28,23 @@ public class Crawler {
     public void crawl(int maxThread) throws InterruptedException {
         init();
 
-        ExecutorService threadPool = Executors.newFixedThreadPool(maxThread);
+        ThreadPoolExecutor threadPool = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
 
         while (true){
             System.out.println("ekhon ache: " + urlQueue.size());
+
             if (urlQueue.isEmpty()) {
-                //if(threadPool.ge)
+                if (threadPool.getActiveCount() == 0)   break;
                 Thread.sleep(1000);
                 continue;
             }
+
             URLTrial top = urlQueue.remove();
             System.out.println(top.url);
             threadPool.execute(new CrawlThread(urlCacheHash, urlCount, urlQueue, top, "./" ));
         }
 
-        //threadPool.shutdown();
+        threadPool.shutdown();
     }
 
     public static void main(String[] args) throws InterruptedException {
--- src/Crawler/ThreadPoolExecutorTest.java
+++ src/Crawler/ThreadPoolExecutorTest.java
@@ -3,9 +3,12 @@ package Crawler;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
+class Summer {
+}
+
 public class ThreadPoolExecutorTest {
     public static void main(String[] args) {
-        //ThreadPoolExecutor threadPoolExecutor = Executors.newFixedThreadPool(10);
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
         System.out.println("Test");
     }
 }
